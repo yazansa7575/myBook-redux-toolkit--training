7488a37f098228e824ae5cebbd2ab79c6674efec
yazan sawan <alisarter2@gmail.com>
2024-02-24 22:47:34 +0300

    .

--- reduxt-toolkit-thunk-book-app-main/src/components/Header.js
+++ reduxt-toolkit-thunk-book-app-main/src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
       <nav className='navbar navbar-dark bg-dark'>
         <span className='navbar-brand mb-0 h1'>My Books</span>
 
-        <button className='btn btn-outline-primary' type='submit' onClick={()=>Dispatch(islogInOut())}>
+        <button className='btn btn-outline-primary' type='submit' onClick={() => Dispatch(islogInOut())}>
           {
             islogin ? "Log Out" : " Log In"
           }
